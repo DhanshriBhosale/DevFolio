b41c5f3bc62906a564b70496cc5b64d2f634a2f9
Dhanshri Bhosale <dhanshribhosale11@gmail.com>
2026-07-29 12:28:36 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      await axios.post("https://devfolio-backend-production-0511.up.railway.app/api/admin/register", {
+      await axios.post("https://devfolio-backend-production-6da2.up.railway.app/api/admin/register", {
         username,
         password,
       });
